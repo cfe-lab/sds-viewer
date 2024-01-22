c9bbf57117c3641e3c37deffb7f335112d55a4d7
Matthias Kalms <mkalms@bccfe.ca>
2024-01-22 10:10:44 -0800

    Set timezone for comparison

--- selfcheck.py
+++ selfcheck.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from pytz import timezone
 
 
 with open('timestamp', 'r') as timestamp:
     updated = datetime.fromisoformat(timestamp.readline())
-    current = datetime.today()
+    current = datetime.now(timezone('America/Vancouver'))
     print((current - updated).days < 8)
